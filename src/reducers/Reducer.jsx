9e69e6eaef23156655c5a548108ae1dd45f4bba2
Dilipraj L. <dilipraj.l@excelindia.com>
2025-05-27 19:20:04 +0530

    using-redux-for-call

--- src/reducers/Reducer.jsx
+++ src/reducers/Reducer.jsx
@@ -1,7 +1,13 @@
+import { FETCh_GAMES_DATA } from "../common/Common";
+
 const initialState = {
     data:[],
 }
 
-export const reducer = (state = initialState, action) =>  {
-
+export const reducer = (state = initialState, action) =>  {        
+    switch(action.type){
+        case FETCh_GAMES_DATA :
+            return {...state, data:action.payload}
+        default : return state;
+    }
 }
