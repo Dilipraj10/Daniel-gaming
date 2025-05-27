b177ed50d17343d032f15c7055c8b528988144ab
Dilipraj L. <dilipraj.l@excelindia.com>
2025-05-27 19:55:59 +0530

    cards-in-rows

--- src/Components/StoreGrids.jsx
+++ src/Components/StoreGrids.jsx
@@ -9,42 +9,67 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchGamesData } from '../actions/Action';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
 
 export default function ResponsiveGrid() {
   const dispatch = useDispatch();
   const gamesData = useSelector(state => state.data)
-  
+
   useEffect(() => {
     dispatch(fetchGamesData())
   }, [dispatch]);
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: (theme.vars ?? theme).palette.text.secondary,
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#1A2027',
+    }),
+  }));
+
   return (
     <>
-      {gamesData && gamesData.map((data) => {
-        return (
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="194"
-              image="/static/images/cards/paella.jpg"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body12" sx={{ color: 'text.secondary' }}>
-                This impressive paella is a perfect party dish and a fun meal to cook
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
-        )
-      })}
+      <Box sx={{ flexGrow: 1, p:4 }}>
+        <Grid container spacing={1}>
+          {gamesData && gamesData.map((data) => {
+            return (
+              <Grid size={3}>
+                <Item>
+                  <Card sx={{ maxWidth: 275 }}>
+                    <CardMedia
+                      component="img"
+                      height="194"
+                      image={data.background_image}
+                      alt={data.name}
+                    />
+                    <CardContent>
+                      <Typography variant="body12" sx={{ color: 'text.secondary' }}>
+                        This impressive paella is a perfect party dish and a fun meal to cook
+                      </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                      <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                      </IconButton>
+                      <IconButton aria-label="share">
+                        <ShareIcon />
+                      </IconButton>
+                    </CardActions>
+                  </Card>
+                </Item>
+              </Grid>
+            )
+          })}
+        </Grid>
+      </Box>
+
     </>
   );
 }
